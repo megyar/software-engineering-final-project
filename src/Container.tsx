c24eee232a2dfcf510a9756be02579d2908a472a
Kristina <kbalta@udel.edu>
2022-12-06 12:23:04 -0500

    idk

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,4 +1,6 @@
 import update from 'immutability-helper'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { listenerCount } from 'process'
 import type { CSSProperties, FC } from 'react'
 import { useCallback, useState } from 'react'
 import type { XYCoord } from 'react-dnd'
@@ -7,6 +9,7 @@ import { useDrop } from 'react-dnd'
 import { Box } from './Box'
 import type { DragItem } from './interfaces'
 import { ItemTypes } from './ItemTypes'
+import { Furniture } from './Components/furniture'
 
 
 //import { chair1 } from './Components/furniture'
@@ -52,21 +55,32 @@ export const Container: FC<ContainerProps> = ({ hideSourceOnDrag }) => {
     },
     [boxes, setBoxes],
   )
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const [continerList, setContainerList] = useState<Furniture>();
+
 
   const [, drop] = useDrop(
     () => ({
       accept: ItemTypes.FBOX, 
       drop(item: DragItem, monitor) {
-         if (monitor.getItemType() === 'fbox') {
+        if (monitor.getItemType() !== 'fbox') {
         const delta = monitor.getDifferenceFromInitialOffset() as XYCoord
         const left = Math.round(item.left + delta.x)
         const top = Math.round(item.top + delta.y)
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const current: Furniture = monitor.getItem();  ////////////////////////////////////////////////////
         moveBox(item.id, left, top)
         return undefined
          }
         else {
-
-          return ;
+         //   setBoxes(
+          //    update(boxes, {
+           //     $merge: {
+          //        
+          //      })
+          //  )
+         //   change type
+         // return ;
       }
 }}),
     [moveBox],
